7bdf25e03f59157b61a170d7a63e96a1a746fc63
leonlai1234 <62087468+leonlai1234@users.noreply.github.com>
2020-04-24 03:41:12 +0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -27,30 +27,14 @@ namespace Snake
 
         public static void WriteAt(string s, int x, int y)
         {
-            try
-            {
                 Console.SetCursorPosition(origCol + x, origRow + y);
                 Console.Write(s);
-            }
-            catch (ArgumentOutOfRangeException e)
-            {
-                Console.Clear();
-                Console.WriteLine(e.Message);
-            }
         }
 
         public static void WriteScore(int s, int x, int y)
         {
-            try
-            {
                 Console.SetCursorPosition(origCol + x, origRow + y);
                 Console.Write(s);
-            }
-            catch (ArgumentOutOfRangeException e)
-            {
-                Console.Clear();
-                Console.WriteLine(e.Message);
-            }
         }
     }
     
